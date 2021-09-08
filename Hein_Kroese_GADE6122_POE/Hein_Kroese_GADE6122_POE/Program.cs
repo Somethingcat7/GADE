a7af1099ffe7326f462e787f9d2c05787c3c45d4
Somethingcat7 <88764947+Somethingcat7@users.noreply.github.com>
2021-09-08 16:51:08 +0200

    Add files via upload

---
+++ Hein_Kroese_GADE6122_POE/Hein_Kroese_GADE6122_POE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hein_Kroese_GADE6122_POE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
