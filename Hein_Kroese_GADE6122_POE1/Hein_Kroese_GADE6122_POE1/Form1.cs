e0657d31fb4c0c485642e9119e974bc262112b59
Somethingcat7 <88764947+Somethingcat7@users.noreply.github.com>
2021-09-08 16:55:18 +0200

    Add files via upload

---
+++ Hein_Kroese_GADE6122_POE1/Hein_Kroese_GADE6122_POE1/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hein_Kroese_GADE6122_POE1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
