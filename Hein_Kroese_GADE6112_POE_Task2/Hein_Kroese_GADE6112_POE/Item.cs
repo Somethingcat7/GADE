bc97bbdbcad2852a152c91cd5c7cd57d4a3dcf99
Somethingcat7 <88764947+Somethingcat7@users.noreply.github.com>
2021-11-02 11:07:47 +0200

    Added more to Item subclass
    Added abstract toString method as well as constructor for the class

---
+++ Hein_Kroese_GADE6112_POE_Task2/Hein_Kroese_GADE6112_POE/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hein_Kroese_GADE6112_POE
+{
+    abstract class Item : Tile
+    {
+        protected Item(int x, int y, char symbol) : base(x, y, symbol)
+        {
+
+        }
+
+        public abstract override string ToString();
+    }
+}
