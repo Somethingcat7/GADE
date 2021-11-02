de3cccc592d6e2ddf831fe43fdee48867072de72
Somethingcat7 <88764947+Somethingcat7@users.noreply.github.com>
2021-11-02 10:43:20 +0200

    Task 2
    This will be the start for task 2 of the POE

---
+++ Hein_Kroese_GADE6112_POE/Hein_Kroese_GADE6112_POE/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hein_Kroese_GADE6112_POE
+{
+    //Subclass Enemy to class Character
+    abstract class Enemy : Character
+    {
+        protected Random RanNum = new Random();
+
+        public int Coords(int min, int max)
+        {
+            return RanNum.Next(min, max);
+        }
+        
+        public Enemy(int x, int y, char symbol, int health, int maxHealth, int damage) : base(x, y, symbol, health, maxHealth, damage)
+        {
+
+        }
+
+        public override MovementEnum ReturnMove()
+        {
+            return MovementEnum.None;
+        }
+
+        
+
+        public override void Attack(Character target)
+        {
+            target.gethealth = target.gethealth - 5;
+        }
+    }
+}
