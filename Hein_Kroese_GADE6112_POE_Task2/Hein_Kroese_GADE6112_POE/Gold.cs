53fa5053ce729b92d62e2b288e97ac21d4fa2ac2
Somethingcat7 <88764947+Somethingcat7@users.noreply.github.com>
2021-11-02 11:24:20 +0200

    Create Gold.cs
    Added Gold subclass with some features such as constructor and random number generator

---
+++ Hein_Kroese_GADE6112_POE_Task2/Hein_Kroese_GADE6112_POE/Gold.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hein_Kroese_GADE6112_POE
+{
+    class Gold : Item
+    {
+        protected Gold(int x, int y, char symbol): base (x,y,symbol)
+        {
+
+        }
+        public override string ToString()
+        {
+            return null;
+        }
+
+        private Random RanDumb = new Random();
+    }
+}
